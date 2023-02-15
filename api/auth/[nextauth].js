491079f53bd9f6251d413dbe1106314154f8920e
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2023-02-15 13:50:38 -0500

    test

--- api/auth/[nextauth].js
+++ api/auth/[nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
-// import { authConfig } from "../../nextauth.config";
-import { authOptions } from "../../src/api/[...nextauth]";
+// import { authOptions } from "../../nextauth.config";
+// import { authOptions } from "../../src/api/[...nextauth]";
 
 
 export default async function handler(req, res) {

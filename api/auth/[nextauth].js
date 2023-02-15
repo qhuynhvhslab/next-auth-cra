24b368c0edeb174d2329ae41b1202965740fa7e8
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2023-02-15 13:45:00 -0500

    test

--- api/auth/[nextauth].js
+++ api/auth/[nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth/next";
-import { authConfig } from "../../nextauth.config";
+// import { authConfig } from "../../nextauth.config";
+import { authOptions } from "../../src/api/[...nextauth]";
+
 
 export default async function handler(req, res) {
   const { nextauth, provider, ...rest } = req.query;
   req.query = { nextauth: [nextauth, provider], ...rest };
-  return await NextAuth(req, res, authConfig);
+  return await NextAuth(req, res, authOptions);
 }
